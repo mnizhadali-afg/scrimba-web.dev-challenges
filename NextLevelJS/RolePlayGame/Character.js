20cb1c0dc38af995ab83fbd0b64513ee88228071
Ali Mousavi <mnizhadali@gmail.com>
2022-04-05 00:15:33 +0500

    Game logic added

--- NextLevelJS/RolePlayGame/Character.js
+++ NextLevelJS/RolePlayGame/Character.js
@@ -1,39 +1,39 @@
-import {getDiceRollArray} from './utils.js'
-
-/*
-CHALLENGE
-1. In the Character constructor, create a new 
-property called "diceArray".
-2. Set diceArray equal to whatever is returned
-by the getDicePlaceholderHtml function (think 
-what two things you need to do to make this work!!).
-3. Instead of rendering diceHtml in the getCharacterHtml 
-method, render diceArray.
-4. Delete any unnecessary code.
-*/
+import { getDiceRollArray, getDicePlaceholderHtml } from './utils.js'
 
 function Character(data) {
     Object.assign(this, data)
     
-    this.getDiceHtml = function(diceCount) {
-        return getDiceRollArray(diceCount).map(function(num){ 
-            return  `<div class="dice">${num}</div>`
-        }).join('')
+    this.diceArray = getDicePlaceholderHtml(this.diceCount)  
+      
+    this.getDiceHtml = function () {
+        this.currentDiceScore = getDiceRollArray(this.diceCount);
+        this.diceArray = this.currentDiceScore.map(function(num){
+            return `<div class="dice">${num}</div>`
+            }).join("")
+    }
+    
+    this.takeDamage = function(attackScoreArray){  
+        const totalAttackScore = attackScoreArray.reduce(function(total, num) {return total + num})
+        this.health -= totalAttackScore
+        if (this.health <= 0){
+            this.dead = true  
+            this.health = 0
+        } 
     }
 
     this.getCharacterHtml = function () {
-        const { elementId, name, avatar, health, diceCount } = this;      
-        let diceHtml = this.getDiceHtml(diceCount);        
-           return `
+        const { elementId, name, avatar, health, diceCount } = this;
+
+        return `
             <div class="character-card">
                 <h4 class="name"> ${name} </h4>
                 <img class="avatar" src="${avatar}" />
                 <div class="health">health: <b> ${health} </b></div>
                 <div class="dice-container">
-                    ${diceHtml}
+                    ${this.diceArray}
                 </div>
             </div>`
-    }  
+    }
 }
 
 export default Character
--- NextLevelJS/RolePlayGame/data.js
+++ NextLevelJS/RolePlayGame/data.js
@@ -2,14 +2,16 @@ const characterData = {
     hero: {
         name: "Wizard",
         avatar: "images/wizard.png",
-        health: 60,
-        diceCount: 3
+        health: 1,
+        diceCount: 3,
+        currentDiceScore: []
     },
     monster: {
         name: "Orc",
         avatar: "images/orc.png",
-        health: 10,
-        diceCount: 1
+        health: 1,
+        diceCount: 1,
+        currentDiceScore: []
     }
 }
 

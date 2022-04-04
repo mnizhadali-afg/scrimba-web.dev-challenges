e6b2ab148b169b167e2b0f8fa84ea06c07523cd0
Ali Mousavi <mnizhadali@gmail.com>
2022-04-05 01:31:37 +0500

    Classes are added

--- NextLevelJS/RolePlayGame/utils.js
+++ NextLevelJS/RolePlayGame/utils.js
@@ -1,11 +1,11 @@
 function getDiceRollArray(diceCount) {
     return new Array(diceCount).fill(0).map(() =>
         Math.floor(Math.random() * 6) + 1
-    );
+    )
 }
 
 const getPercentage = (remainingHealth, maximumHealth) => 
-    (100 * remainingHealth) / maximumHealth;
+    (100 * remainingHealth) / maximumHealth
 
 function getDicePlaceholderHtml(diceCount) {
     return new Array(diceCount).fill(0).map(() =>
